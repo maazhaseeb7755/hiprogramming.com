1c34855c48856589ceced6d6e0a67ea32b280334
maazhaseeb7755 <72330390+maazhaseeb7755@users.noreply.github.com>
2022-06-06 11:05:17 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hi- Fist Program in PHP| www.hiprogramming.com</title>
+</head>
+
+<body>
+<?php 
+echo "<h3>Hi - Its my First Program in PHP!</h3></br>";
+echo "<h4>Created by www.hiprogramming.com </h4>";
+ ?>
+</body>
+</html>
