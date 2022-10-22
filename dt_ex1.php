b076155d4f67b95a6ccd51b6cd945abc23b8cf36
maazhaseeb7755 <72330390+maazhaseeb7755@users.noreply.github.com>
2022-10-22 14:23:39 +0500

    Add files via upload

---
+++ dt_ex1.php
@@ -0,0 +1,25 @@
+<!--www.hi2porgramming.com
+PHP Varaiables 
+  -->
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Data Types in PHP</title>
+</head>
+
+<body>
+<?php 
+ //declare vaiables name,age,salary, employed
+ $name="M Khalil";
+ $age=25;
+ $salary=25000.00;
+ $is_employed=TRUE;
+ echo "Displaying the Data</br>";
+ echo "Name is =$name </br>";
+ echo "Age is is =$age </br>";
+ echo "Salary is =$salary </br>";
+ echo "Are you Employed =$is_employed</br>";
+?>
+</body>
+</html>
