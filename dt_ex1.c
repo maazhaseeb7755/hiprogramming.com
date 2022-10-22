aa024379231fcd73035924f0c4c594743e5c8d83
maazhaseeb7755 <72330390+maazhaseeb7755@users.noreply.github.com>
2022-10-22 14:24:00 +0500

    Add files via upload

---
+++ dt_ex1.c
@@ -0,0 +1,18 @@
+/* www.hi2programming.com */
+
+#include <stdio.h>
+
+int main(){
+	int age;
+	double salary;
+	char name[20] = "M Khalil";
+	age=25;
+	salary=25000.00;
+	
+	printf("Name is =%s\n",name);
+	printf("Age is =%d\n",age);
+	printf("Salary is =%f\n",salary);
+	
+	
+return 0;
+}
