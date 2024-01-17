e20093ddbce6bd2923f11b93cd32a2988104369e
maazhaseeb7755 <haseebseo727@gmail.com>
2024-01-17 10:50:48 +0500

    New Files

---
+++ bg.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Background</title>
+<style>
+body{background-image:url(office.jpeg); background-attachment:fixed; }
+</style>
+</head>
+
+<body>
+
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+...
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+...
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+....
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+...<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+...<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+<h2>Hi There...</h2>
+
+</body>
+</html>
